e16e2ebc8494a7e23e0da8fe51bffd940bc07607
Renato Filho <renatocrvg@info.ufrn.br>
2020-07-31 09:54:07 -0300

    feat: show images in result bmn
    This feature show images in result scren.

--- lib/ui/CalculatorIMC.dart
+++ lib/ui/CalculatorIMC.dart
@@ -26,33 +26,32 @@ class _CalculatorIMCState extends State<CalculatorIMC> {
   void _calcular() {
 //    setState(() {});
     double peso = double.parse(pesoController.text);
-    double altura = double.parse(alturaController.text)/100;
+    double altura = double.parse(alturaController.text) / 100;
     double imc = peso / (altura * altura);
 
-    if(imc < 18.6) {
-      _textInfo = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
-      _imagem = "images/magro.jpg";
-    } else if(imc > 18.6 && imc < 24.9) {
+    if (imc < 18.6) {
+      _textInfo = "Abaixo do peso iii (${imc.toStringAsPrecision(4)})";
+      _imagem = "images/thin-man.png";
+    } else if (imc > 18.6 && imc < 24.9) {
       _textInfo = "Peso ideal (${imc.toStringAsPrecision(4)})";
-      _imagem = "images/peso_ideal.jpg";
-    } else if(imc > 24.9 && imc < 29.9) {
+      _imagem = "images/medium-man.png";
+    } else if (imc > 24.9 && imc < 29.9) {
       _textInfo = "Levemente acima do peso s (${imc.toStringAsPrecision(4)})";
-      _imagem = "images/levemente_acima_peso.jpg";
-    } else if(imc > 29.9 && imc < 34.9) {
+      _imagem = "images/little-man.png";
+    } else if (imc > 29.9 && imc < 34.9) {
       _textInfo = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
-      _imagem = "images/obeso.jpg";
-    } else if(imc > 34.9 && imc < 39.9) {
+      _imagem = "images/fat-man.png";
+    } else if (imc > 34.9 && imc < 39.9) {
       _textInfo = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
-      _imagem = "images/obeso.jpg";
-    } else if(imc > 39.9) {
+      _imagem = "images/fat-man.png";
+    } else if (imc > 39.9) {
       _textInfo = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
-      _imagem = "images/obeso.jpg";
+      _imagem = "images/big-fat-man.png";
     }
 
-    Navigator.push(context, MaterialPageRoute(builder: (context) => Result(_imagem, _textInfo)));
-
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => Result(_imagem, _textInfo)));
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -86,7 +85,7 @@ class _CalculatorIMCState extends State<CalculatorIMC> {
                   style: TextStyle(color: Colors.red, fontSize: 25.0),
                   controller: pesoController,
                   validator: (value) {
-                    if(value.isEmpty) return "Insira seu peso!";
+                    if (value.isEmpty) return "Insira seu peso!";
                   },
                 ),
                 TextFormField(
@@ -98,7 +97,7 @@ class _CalculatorIMCState extends State<CalculatorIMC> {
                   style: TextStyle(color: Colors.red, fontSize: 25.0),
                   controller: alturaController,
                   validator: (value) {
-                    if(value.isEmpty) return "Insira sua altura!";
+                    if (value.isEmpty) return "Insira sua altura!";
                   },
                 ),
                 Padding(
@@ -107,9 +106,11 @@ class _CalculatorIMCState extends State<CalculatorIMC> {
                       height: 50.0,
                       child: RaisedButton(
                         onPressed: () {
-                          if(_formKey.currentState.validate()) _calcular();
+                          if (_formKey.currentState.validate()) _calcular();
                         },
-                        child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        child: Text(
+                          "Calcular",
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
                         ),
                         color: Colors.red,
                       )),
@@ -122,7 +123,6 @@ class _CalculatorIMCState extends State<CalculatorIMC> {
               ],
             ),
           ),
-        )
-    );
+        ));
   }
 }
